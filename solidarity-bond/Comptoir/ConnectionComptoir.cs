ee8001f53ea7e948d9c930642441767c97141468
Alexis BAUDRY <alexis.baudry@viacesi.fr>
2020-06-11 14:56:51 +0200

    add package

--- solidarity-bond/Comptoir/ConnectionComptoir.cs
+++ solidarity-bond/Comptoir/ConnectionComptoir.cs
@@ -4,7 +4,7 @@ namespace solidarity_bond
 {
     class ConnectionComptoir
     {
-        static void Main(string[] args)
+        static void zza(string[] args)
         {
             Console.WriteLine("Lancement application");
             Service service = new Service();
